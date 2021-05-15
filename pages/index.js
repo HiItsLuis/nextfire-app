c0edc561af5f2b346e2296d7b9465e30c50e19bf
HiItsLuis <luisferponfe@gmail.com>
2021-05-15 12:28:18 +0200

    ✨ SSR + Paginated Home Page Feed

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,72 @@
 import Loader from '../components/Loader'
 import toast from 'react-hot-toast'
+import { firestore, postToJSON, fromMillis } from '../lib/firebase'
+import { useState } from 'react'
+import PostFeed from '../components/PostFeed'
 
-export default function Home () {
+const POSTS_LIMIT = 1
+
+export async function getServerSideProps (context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(POSTS_LIMIT)
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON)
+
+  return {
+    props: { posts }
+  }
+}
+
+export default function Home (props) {
+  const [posts, setPosts] = useState(props.posts)
+  const [loading, setLoading] = useState(false)
+
+  const [postsEnd, setPostsEnd] = useState(false)
+
+  const getMorePosts = async () => {
+    setLoading(true)
+    const last = posts[posts.length - 1]
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(POSTS_LIMIT)
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data())
+
+    setPosts(posts.concat(newPosts))
+    setLoading(false)
+
+    if (newPosts.length < POSTS_LIMIT) {
+      setPostsEnd(true)
+    }
+  }
+
   return (
-    <>
+    <main>
       <h1>Home</h1>
-      <Loader show />
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end'}
+
       <div>
         <button onClick={() => toast.success('Hello toast!')}>
-          Toast me
-        </button>
+            Toast me
+          </button>
       </div>
-    </>
+
+    </main>
 
   )
 }
