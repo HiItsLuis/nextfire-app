f1eac5c5ba3131be82fd6611a481e5fcd498a714
HiItsLuis <luisferponfe@gmail.com>
2021-05-17 11:01:37 +0200

    💄Add 404 Page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -7,6 +7,8 @@ export async function getServerSideProps ({ query }) {
 
   const userDoc = await getUserWithUserName(userName)
 
+  if (!userDoc) return { notFound: true }
+
   // JSON serializable data
   let user = null
   let posts = null
